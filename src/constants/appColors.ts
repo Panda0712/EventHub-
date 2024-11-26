2466f8031d1d11573c9f1e010da842f08cf71243
Pham Tuan <tuan.pham.super@gmail.com>
2024-11-27 00:16:13 +0700

    first commit

---
+++ src/constants/appColors.ts
@@ -0,0 +1,7 @@
+export const appColors = {
+  primary: '#5669ff',
+  white: '#ffffff',
+  text: '#120d26',
+  grey: '#807a7a',
+  grey2: '#dadada',
+};
---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+import LoginScreen from './auth/LoginScreen';
+import SignUpScreen from './auth/SignUpScreen';
+import HomeScreen from './home/HomeScreen';
+import SplashScreen from './SplashScreen';
+
+export {HomeScreen, SignUpScreen, LoginScreen, SplashScreen};
